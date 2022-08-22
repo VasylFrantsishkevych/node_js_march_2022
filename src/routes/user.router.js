325f8b6f1b870dbc9b5f2b43e681999f47706800
VF <vasul.fr@gmail.com>
2022-08-22 18:02:03 +0300

    вввв

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 
-const userController = require("../controllers/user.controller");
-const userMiddelware = require('../middlewares/user.middelware');
+const { userController } = require("../controllers");
+const userMiddleware = require('../middlewares/user.middleware');
 
 const userRouter = Router();
 
 userRouter.get('/', userController.getAllUsers)
-userRouter.post('/',userMiddelware.checkIsUserBodyValid, userController.createUser)
+userRouter.post('/',userMiddleware.checkIsUserBodyValid, userController.createUser)
 userRouter.get('/:userId', userController.getUserById)
 userRouter.put('/:userId', userController.updateUserByID)
 userRouter.delete('/:userId', userController.deleteUserById)
