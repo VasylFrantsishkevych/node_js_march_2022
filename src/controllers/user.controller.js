f35e8cafe1b9c1c9a9528813f39b5302f02a5a1f
VF <vasul.fr@gmail.com>
2022-08-27 11:22:49 +0300

    index

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,70 +1,91 @@
 const fileService = require("../services/file.service");
+const {statusCode} = require("../constants");
 
 module.exports = {
     getAllUsers: async (req, res) => {
-        const usersFromService = await fileService.getUser();
-        res.json(usersFromService)
+        try {
+            const usersFromService = await fileService.getUser();
+            res.json(usersFromService)
+        } catch (e) {
+            res.status(400).json({ message: e.message})
+        }
+
     },
     createUser: async (req, res) => {
+        try {
+            const setNewUser = await fileService.insertUser(req.body);
+            res.status(statusCode.CREATE).json(setNewUser)
+        } catch (e) {
+            res.json(e)
+        }
 
-        const setNewUser = await fileService.insertUser(req.body);
-
-        res.status(201).json(setNewUser)
     },
     getUserById: async (req, res) =>{
         const {userId} = req.params;
+            try {
+                if (Number.isNaN(+userId) || +userId < 0) {
+                    res.status(statusCode.BAD_REQUEST).json('Wrong user Id')
+                    return;
+                }
 
-        if (Number.isNaN(+userId) || +userId < 0) {
-            res.status(400).json('Wrong user Id')
-            return;
-        }
+                const user = await fileService.getById(+userId);
 
-        const user = await fileService.getById(+userId);
+                if (!user) {
+                    res.status(statusCode.NOT_FOUND).json('User not found')
+                    return;
+                }
 
-        if (!user) {
-            res.status(404).json('User not found')
-            return;
-        }
+                res.json(user)
+            } catch (e) {
+                res.json(e)
+            }
 
-        res.json(user)
     },
     updateUserByID: async (req, res) => {
         const {userId} = req.params;
         const {age, name} = req.body;
+            try {
+                if (Number.isNaN(+userId) || +userId < 0) {
+                    res.status(statusCode.BAD_REQUEST).json('Wrong user Id')
+                    return;
+                }
 
-        if (Number.isNaN(+userId) || +userId < 0) {
-            res.status(400).json('Wrong user Id')
-            return;
-        }
+                const userObject = {};
+                if (age) userObject.age = age;
+                if (name) userObject.name = name;
 
-        const userObject = {};
-        if (age) userObject.age = age;
-        if (name) userObject.name = name;
+                const user = await fileService.updateById(+userId, req.body);
 
-        const user = await fileService.updateById(+userId, req.body);
+                if (!user) {
+                    res.status(statusCode.NOT_FOUND).json('User not found')
+                    return;
+                }
 
-        if (!user) {
-            res.status(404).json('User not found')
-            return;
-        }
+                res.status(statusCode.CREATE).json(user);
+            } catch (e) {
+                res.json(e)
+            }
 
-        res.status(201).json(user);
     },
     deleteUserById: async (req, res) => {
         const {userId} = req.params;
+            try {
+                if (Number.isNaN(+userId) || +userId < 0) {
+                    res.status(statusCode.BAD_REQUEST).json('Wrong user Id')
+                    return;
+                }
 
-        if (Number.isNaN(+userId) || +userId < 0) {
-            res.status(400).json('Wrong user Id')
-            return;
-        }
+                const user = await fileService.deleteById(+userId);
 
-        const user = await fileService.deleteById(+userId);
+                if (!user) {
+                    res.status(statusCode.NOT_FOUND).json('User not found')
+                    return;
+                }
 
-        if (!user) {
-            res.status(404).json('User not found')
-            return;
-        }
+                res.sendStatus(statusCode.NO_CONTENT);
+            } catch (e) {
+                res.json(e)
+            }
 
-        res.sendStatus(204);
     }
 }
